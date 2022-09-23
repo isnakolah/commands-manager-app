ea491359c8434b451afde8379bc4f634ef1c3fa8
isnakolah <nakolahdaniel1@gmail.com>
2022-09-23 10:17:04 +0300

    Create CommandService with a post endpoint to add commands

---
+++ src/CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers;
+
+[ApiController]
+[Route("api/c/[controller]")]
+public class PlatformsController : ControllerBase
+{
+    [HttpPost]
+    public ActionResult TestInboundConnection()
+    {
+        Console.WriteLine("--> Inbound POST # CommandService");
+
+        return Ok("Inbound test of from Platforms Controller");
+    }
+}
