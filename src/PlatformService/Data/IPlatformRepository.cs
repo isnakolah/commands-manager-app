e4c0d456de17c8b8c2e5d8a80ade07aae85d35a8
isnakolah <nakolahdaniel1@gmail.com>
2022-09-25 00:03:06 +0300

    Add projections too the repository

--- src/PlatformService/Data/IPlatformRepository.cs
+++ src/PlatformService/Data/IPlatformRepository.cs
@@ -1,11 +1,14 @@
-﻿using PlatformService.Models;
+﻿using PlatformService.DTOs;
+using PlatformService.Models;
 
 namespace PlatformService.Data;
 
 public interface IPlatformRepository
 {
     Task<IEnumerable<Platform>> GetAllAsync();
+    Task<IEnumerable<PlatformReadDTO>> GetAllDTOsAsync();
     Task<Platform?> GetByIdAsync(int id);
+    Task<PlatformReadDTO?> GetDTOByIdAsync(int id);
     void Create(Platform platform);
     Task<bool> SaveChangesAsync();
 }
--- src/PlatformService/Data/PlatformRepository.cs
+++ src/PlatformService/Data/PlatformRepository.cs
@@ -1,14 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using PlatformService.DTOs;
 using PlatformService.Models;
+using IConfigurationProvider = AutoMapper.IConfigurationProvider;
 
 namespace PlatformService.Data;
 
 public sealed class PlatformRepository : IPlatformRepository
 {
+    private readonly IConfigurationProvider _mapperConfig;
     private readonly AppDbContext _context;
 
-    public PlatformRepository(AppDbContext context)
+    public PlatformRepository(IConfigurationProvider mapperConfig, AppDbContext context)
     {
+        _mapperConfig = mapperConfig;
         _context = context;
     }
 
@@ -17,9 +22,28 @@ public sealed class PlatformRepository : IPlatformRepository
         return await _context.Platforms.ToArrayAsync();
     }
 
+    public async Task<IEnumerable<PlatformReadDTO>> GetAllDTOsAsync()
+    {
+        var platformDTOs = await _context.Platforms
+            .ProjectTo<PlatformReadDTO>(_mapperConfig)
+            .ToArrayAsync();
+
+        return platformDTOs;
+    }
+
     public async Task<Platform?> GetByIdAsync(int id)
     {
         return await _context.Platforms.FindAsync(id);
+    }
+
+    public async Task<PlatformReadDTO?> GetDTOByIdAsync(int id)
+    {
+        var platformDTO = await _context.Platforms
+            .Where(x => x.Id == id)
+            .ProjectTo<PlatformReadDTO>(_mapperConfig)
+            .FirstOrDefaultAsync();
+
+        return platformDTO;
     }
 
     public void Create(Platform platform)
